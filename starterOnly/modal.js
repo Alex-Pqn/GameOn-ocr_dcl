aa8c97beaee326ef4ed424d2186376b011eebe5d
Alexandre P <@paquienalexandre0@gmail.com>
2021-11-22 02:50:22 +0100

    issue fix: close opened modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.getElementById('modal-btn-close')
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtnClose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
